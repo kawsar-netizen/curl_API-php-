155117d44bcf2986988db32a3f325a49b1d04a8c
sizar <sizar@gmail.com>
2022-06-21 15:41:24 +0600

    Syntax for cURL

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-index.php
+<?php
+
+// curl_init();
+// curl_setopt();
+// curl_exec();
+// curl_close();
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/");
+curl_exec($ch);
+curl_close($ch);
+
+?>
